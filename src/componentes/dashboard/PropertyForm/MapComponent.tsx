f3bd614100eabddbc1a6db81c8792dc367c43730
Nejosudo <nejosudo2024@gmail.com>
2026-08-30 19:48:40 -0500

    fix: update Content Security Policy and improve Leaflet marker icon URLs

--- src/componentes/dashboard/PropertyForm/MapComponent.tsx
+++ src/componentes/dashboard/PropertyForm/MapComponent.tsx
@@ -6,12 +6,11 @@ import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 import CotsemLayers from '@/componentes/ui/CotsemLayers/CotsemLayers';
 
-// Fix for default marker icons in Leaflet with Next.js
 delete (L.Icon.Default.prototype as any)._getIconUrl;
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
-  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
-  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
 });
 
 interface MapComponentProps {
